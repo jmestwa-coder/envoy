f345be4c0e30479da11d63b0700da3b1b0dea311
jmestwa-coder <jmestwa@gmail.com>
2026-04-28 23:30:17 +0530

    replace raw pointer Hex::encode API with absl::Span and update call sites
    Signed-off-by: jmestwa-coder <jmestwa@gmail.com>

--- source/extensions/filters/network/mongo_proxy/bson_impl.cc
+++ source/extensions/filters/network/mongo_proxy/bson_impl.cc
@@ -353,7 +353,7 @@ std::string FieldImpl::toString() const {
 
   case Type::ObjectId: {
     return fmt::format("\"{}\"",
-                       Hex::encode(&value_.object_id_value_[0], value_.object_id_value_.size()));
+                       Hex::encode(value_.object_id_value_));
   }
 
   case Type::Boolean: {
